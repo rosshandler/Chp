37e23791e803a014e239a18e4b57fa7b5ec04fba
rosshandler <ivanir@mrc-lmb.cam.ac.uk>
2022-06-21 15:59:07 +0100

    Update cellranger_commands.sh

--- 10X/cellranger_commands.sh
+++ 10X/cellranger_commands.sh
@@ -1,11 +1,11 @@
 cellranger count \
---id=ChPH1D19 \
+--id=ChPH1D19_A \
 --transcriptome=/data2/genome_builds/refdata-cellranger-GRCh38-1.2.0/ \
 --fastqs=/data1/ivanir/Chp2022/10xGen/fastqs/ --localcores=40 \
---sample=SLX-21922.SITTA1,SLX-21922.SITTB1 --localmem=48
+--sample=SITTA1 --localmem=48
 
 cellranger count \
---id=ChPH1D19 \
+--id=ChPH1D19_B \
 --transcriptome=/data2/genome_builds/refdata-cellranger-GRCh38-1.2.0/ \
 --fastqs=/data1/ivanir/Chp2022/10xGen/fastqs/ --localcores=40 \
---sample=SLX-21922.SITTA1 --localmem=48
+--sample=SITTB1 --localmem=48
