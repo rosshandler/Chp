3b036ddd3475b719098f9b9d44796c722fc3f809
rosshandler <ivanir@mrc-lmb.cam.ac.uk>
2022-06-29 14:05:29 +0100

    Update spipe_commands.sh

--- ParseBioSciences/spipe_commands.sh
+++ ParseBioSciences/spipe_commands.sh
@@ -15,3 +15,8 @@ split-pipe --mode all --kit WT_mini --genome_dir $PBS/newvolume/genomes/hg38/ \
 --fq1 $PBS/newvolume/expdata/SLX-21921.DNAA007.H52HVDRX2.s_2.r_1.fq.gz \
 --fq2 $PBS/newvolume/expdata/SLX-21921.DNAA007.H52HVDRX2.s_2.r_2.fq.gz \
 --output_dir $PBS/newvolume/analysis/DNAA007 
+
+split-pipe --mode all --kit WT_mini --genome_dir $PBS/newvolume/genomes/hg38/ \
+--fq1 $PBS/newvolume/expdata/SLX-21921.DNAA008.H52HVDRX2.s_2.r_1.fq.gz \
+--fq2 $PBS/newvolume/expdata/SLX-21921.DNAA008.H52HVDRX2.s_2.r_2.fq.gz \
+--output_dir $PBS/newvolume/analysis/DNAA008 
