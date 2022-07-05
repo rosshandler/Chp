adbb42c6b2dd0aecbd1ad8401f1337b43e2d3981
rosshandler <ivanir@mrc-lmb.cam.ac.uk>
2022-07-05 13:05:28 +0100

    Update spipe_commands.sh

--- ParseBioSciences/spipe_commands.sh
+++ ParseBioSciences/spipe_commands.sh
@@ -1,9 +1,10 @@
 # spipe is already installed as conda env
 conda activate spipe
 
-# Genome indexing
+
 PBS='/data1/ivanir/Chp2022/ParseBS'
 
+# Genome indexing
 split-pipe \
 --mode mkref \
 --genome_name hg38 \
@@ -11,12 +12,19 @@ split-pipe \
 --genes $PBS/newvolume/genomes/Homo_sapiens.GRCh38.93.gtf.gz \
 --output_dir $PBS/newvolume/genomes/hg38
 
+# Pipeline running 
 split-pipe --mode all --kit WT_mini --genome_dir $PBS/newvolume/genomes/hg38/ \
 --fq1 $PBS/newvolume/expdata/SLX-21921.DNAA007.H52HVDRX2.s_2.r_1.fq.gz \
 --fq2 $PBS/newvolume/expdata/SLX-21921.DNAA007.H52HVDRX2.s_2.r_2.fq.gz \
---output_dir $PBS/newvolume/analysis/DNAA007 
+--output_dir $PBS/newvolume/analysis/DNAA008 
 
 split-pipe --mode all --kit WT_mini --genome_dir $PBS/newvolume/genomes/hg38/ \
 --fq1 $PBS/newvolume/expdata/SLX-21921.DNAA008.H52HVDRX2.s_2.r_1.fq.gz \
 --fq2 $PBS/newvolume/expdata/SLX-21921.DNAA008.H52HVDRX2.s_2.r_2.fq.gz \
 --output_dir $PBS/newvolume/analysis/DNAA008 
+
+# Combine outputs
+split-pipe \
+    --mode comb \
+    --sublibraries $PBS/newvolume/analysis/DNAA007 $PBS/newvolume/analysis/DNAA008 \
+    --output_dir $PBS/newvolume/analysis/Combined
